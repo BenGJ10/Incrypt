3f99a2262f26a0185e4b2dc4706e23b00653c73a
BenGJ10 <bengj1015@gmail.com>
2026-02-02 00:46:08 +0530

    Initial files

---
+++ src/test/java/com/bengregory/notes/IncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bengregory.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncryptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
