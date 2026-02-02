534f7b3478a636e9e55c8c3fe8ea8bd52f2a618b
BenGJ10 <bengj1015@gmail.com>
2026-02-02 20:20:41 +0530

    update: implemented notes crud endpoints

--- src/main/java/com/bengregory/notes/service/NoteService.java
+++ src/main/java/com/bengregory/notes/service/NoteService.java
@@ -2,29 +2,43 @@ package com.bengregory.notes.service;
 
 import java.util.List;
 import com.bengregory.notes.model.Note;
+import com.bengregory.notes.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class NoteService implements INoteService{
 
+    @Autowired
+    private NoteRepository noteRepository;
+
     @Override
     public Note createNoteForUser(String username, String content) {
-        return null;
+        Note note = new Note();
+        note.setContent(content);
+        note.setUsername(username);
+        return noteRepository.save(note);
     }
 
     @Override
     public Note updateNoteForUser(Long noteId, String content, String username) {
-        return null;
+        Note note = noteRepository.findById(noteId).orElseThrow(
+                () -> new RuntimeException("Note not found in the db!"));
+        note.setContent(content);
+        return noteRepository.save(note);
     }
 
     @Override
     public void deleteNoteForUser(Long noteId, String username) {
-
+        Note note = noteRepository.findById(noteId).orElseThrow(
+                () -> new RuntimeException("Note not found in the db!"));
+        noteRepository.delete(note);
     }
 
     @Override
     public List<Note> getNotesForUser(String username) {
-        return List.of();
+        List<Note> notesForUser = noteRepository.findByOwnerUsername(username);
+        return notesForUser;
     }
 }
