230c3753643258aef396db67246edcc62721bdc3
BenGJ10 <bengj1015@gmail.com>
2026-02-05 14:16:23 +0530

    feat: added Custom UserDetails implementation

---
+++ src/main/java/com/bengregory/notes/security/services/UserDetailsImplementation.java
@@ -0,0 +1,113 @@
+package com.bengregory.notes.security.services;
+import com.bengregory.notes.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+
+@Data
+@NoArgsConstructor
+public class UserDetailsImplementation implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+    @JsonIgnore
+    private String password;
+    private boolean is2faEnabled;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImplementation(Long id, String username, String email, String password, boolean is2faEnabled, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.is2faEnabled = is2faEnabled;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImplementation build(User user){
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getAppRole().name());
+
+        return new UserDetailsImplementation(
+                user.getUserId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.isTwoFactorEnabled(),
+                List.of(authority) // Create a list with a single authority
+        );
+    }
+
+    // Getters for UserDetails interface
+    
+    @Override
+    // Collection of authorities (roles/permissions) granted to the user
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    public boolean isIs2faEnabled(){
+        return is2faEnabled;
+    }
+
+    @Override
+    // Override equals to compare users based on their ID
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImplementation user = (UserDetailsImplementation) o;
+        return Objects.equals(id, user.id);
+    }
+}
+
+
