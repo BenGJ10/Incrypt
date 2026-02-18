84c1e36369eb70376645b4c096f8f84e8e7c20b1
BenGJ10 <bengj1015@gmail.com>
2026-02-18 14:53:15 +0530

    update: Auth method for Signup

--- src/main/java/com/bengregory/notes/controller/AuthController.java
+++ src/main/java/com/bengregory/notes/controller/AuthController.java
@@ -1,9 +1,17 @@
 package com.bengregory.notes.controller;
 
+import com.bengregory.notes.model.AppRole;
+import com.bengregory.notes.model.Role;
+import com.bengregory.notes.model.User;
+import com.bengregory.notes.repository.RoleRepository;
+import com.bengregory.notes.repository.UserRepository;
 import com.bengregory.notes.security.jwt.JWTUtils;
 import com.bengregory.notes.security.request.LoginRequest;
 
+import com.bengregory.notes.security.request.SignupRequest;
 import com.bengregory.notes.security.response.LoginResponse;
+import com.bengregory.notes.security.response.MessageResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +21,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -32,6 +40,15 @@ public class AuthController {
 
     @Autowired
     private AuthenticationManager authManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
 
     // Method for handling user authentication requests
     @PostMapping("/public/signin")
@@ -69,6 +86,54 @@ public class AuthController {
         LoginResponse response = new LoginResponse(jwtToken, userDetails.getUsername(), roles);
         
         return ResponseEntity.ok(response);
+    }
+
+    // Method for handling user registration requests
+    @PostMapping("/public/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest){
+        
+        // Checking if the username or email provided in the signup request already exists in the database
+        if(userRepository.existsByUserName(signupRequest.getUsername())){
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+        }
+        if(userRepository.existsByEmail(signupRequest.getEmail())){
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new User account
+        User user = new User(signupRequest.getUsername(), signupRequest.getEmail(),
+                encoder.encode(signupRequest.getPassword())); // Encoding the password
+
+        Set<String> strRoles = signupRequest.getRole();
+        Role role;
+
+        // If no roles are specified in the signup request, assign the default role of ROLE_USER to the new user
+        if(strRoles == null || strRoles.isEmpty()){
+            role = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role not found!"));
+        }
+        else{
+            String roleStr = strRoles.iterator().next();
+            if(roleStr.equals("admin")){
+                role = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                        .orElseThrow(() -> new RuntimeException("Error: Role not found!"));
+            }
+            else{
+                role = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                        .orElseThrow(() -> new RuntimeException("Error: Role not found!"));
+            }
+            user.setAccountNonLocked(true);
+            user.setAccountNonExpired(true);
+            user.setCredentialsNonExpired(true);
+            user.setEnabled(true);
+            user.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+            user.setAccountExpiryDate(LocalDate.now().plusYears(1));
+            user.setTwoFactorEnabled(false);
+            user.setSignUpMethod("email");
+        }
+        user.setRole(role);
+        userRepository.save(user);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
 
 }
--- src/main/java/com/bengregory/notes/repository/UserRepository.java
+++ src/main/java/com/bengregory/notes/repository/UserRepository.java
@@ -13,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByUserName(String username);
 
+    Boolean existsByEmail(String email);
 }
