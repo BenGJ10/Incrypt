95604819a69aee20427ea3c509e8d105dbbd2393
BenGJ10 <bengj1015@gmail.com>
2026-02-19 16:25:07 +0530

    chore: add documentation comment

--- src/main/java/com/bengregory/notes/security/response/MessageResponse.java
+++ src/main/java/com/bengregory/notes/security/response/MessageResponse.java
@@ -7,6 +7,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+
+/* Using a custom MessageResponse class allows us to structure our response in a consistent way, making it easier for clients 
+to parse and understand the response. It also allows us to include additional information in the response,
+such as status codes or error details, if needed in the future. This approach promotes better maintainability and scalability of our API. */
+
 public class MessageResponse {
     private String message;
 }
