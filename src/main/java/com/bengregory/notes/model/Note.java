3283e52e83c5f6bfa3905882c7d74a4445324262
BenGJ10 <bengj1015@gmail.com>
2026-03-17 23:45:05 +0530

    update: added timestamps for Note UI fix

--- src/main/java/com/bengregory/notes/model/Note.java
+++ src/main/java/com/bengregory/notes/model/Note.java
@@ -2,6 +2,10 @@ package com.bengregory.notes.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -15,4 +19,10 @@ public class Note {
     private String content;
 
     private String username;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
